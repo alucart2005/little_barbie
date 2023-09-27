b90d73af5ca6e0682df2558bf560379a5040c524
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-27 18:34:49 -0500

    feature: end

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+  navClose = document.getElementById("nav-close"),
+  navLink = document.querySelectorAll(".nav__link");
 
-  if(navToggle){
-    navToggle.addEventListener("click", ()=>{
-      navMenu.classList.add("show-menu")
-    })
-  }
-  if(navClose){
-    navClose.addEventListener("click", ()=>{
-      navMenu.classList.remove("show-menu")
-    })
-  }
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((item) => item.addEventListener("click", linkAction));
+
+//SCROLL REVEAL
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(`.button,.newsletter__container`);
+sr.reveal(`.category__data,.footer__content`, { interval: 100 });
+sr.reveal(`.swiper-slide`, { origin: "left" });
+
+//SCROLL UP
+//function scrollUp()
