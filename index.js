280994c621ac7781516619c8a88a4b431595406f
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-30 08:14:45 -0500

    faeture: end of the project 😁

--- index.js
+++ index.js
@@ -32,4 +32,9 @@ sr.reveal(`.category__data,.footer__content`, { interval: 100 });
 sr.reveal(`.swiper-slide`, { origin: "left" });
 
 //SCROLL UP
-//function scrollUp()
+function scrollUp() {
+  const scrollup = document.getElementById("scroll-up");
+  if (this.scrollY >= 60) scrollup.classList.add("show-scroll");
+  else scrollup.classList.remove("show-scroll");
+}
+window.addEventListener(`scroll`, scrollUp);
